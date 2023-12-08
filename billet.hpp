bfcc057f3f07d58ae811d7731e3bac260322ebcb
nic0119 <152968619+nic0119@users.noreply.github.com>
2023-12-08 11:39:43 +0100

    Add files via upload

--- billet.hpp
+++ billet.hpp
@@ -6,43 +6,17 @@
 class Billet{
     private:
         int numeroBillet;
-        std::string dateVol;
-        std::string paysDepart;
-        std::string paysArrivee;
-        double prix;
         std::string typeClasse;
-        int terminalDepart;
-        int terminalArrivee;
         int siegePassager;
     
     public:
-        Billet(int _numeroBillet, std::string _date, std::string _paysDepart, std::string _paysArrivee, double _prix, std::string _typeClasse, int _terminalDepart, int _terminalArrivee, int _siegePassager);
+        Billet(int _numeroBillet, std::string _typeClasse, int _siegePassager);
 
         void setNumeroBillet(int _numeroBillet);
         int getNumeroBillet() const;
 
-
-        void setDateVol(std::string _date);
-        std::string getDateVol() const;
-
-
-        void setPaysDepart(std::string _paysDepart);
-        std::string getPaysDepart() const;
-
-        void setPaysArrivee(std::string _paysArrivee);
-        std::string getPaysArrivee() const;
-        
-        void setPrix(double _prix);
-        double getPrix() const;
-
         void setTypeClasse(std::string _typeClasse);
         std::string getTypClasse() const;
-
-        void setTerminalDepart(int _terminalDepart);
-        int getTerminalDepart() const;
-
-        void setTerminalArrivee(int _terminalArrivee);
-        int getTerminalArrivee() const;
 
         void setSiegePassager(int _siegePassager);
         int getSiegePassager() const;
