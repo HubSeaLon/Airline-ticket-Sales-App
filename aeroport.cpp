77f864f1816786b1a2e19f55ae49c0a737a9cef6
TimothyMarcia <152968262+TimothyMarcia@users.noreply.github.com>
2023-12-10 19:15:15 +0100

    Add files via upload
    Ajout .cpp classe aeroport

---
+++ aeroport.cpp
@@ -0,0 +1,37 @@
+#include "Aeroport.hpp"
+
+// Constructeur par défaut
+Aeroport::Aeroport() : nombreTerminal(0) {}
+
+// Méthodes de modification des attributs
+void Aeroport::setNom(const std::string& newNom) {
+    nom = newNom;
+}
+
+void Aeroport::setAdresse(const std::string& newAdresse) {
+    adresse = newAdresse;
+}
+
+void Aeroport::setNombreTerminal(int newNombreTerminal) {
+    nombreTerminal = newNombreTerminal;
+}
+
+// Méthodes d'accès aux attributs
+std::string Aeroport::getNom() const {
+    return nom;
+}
+
+std::string Aeroport::getAdresse() const {
+    return adresse;
+}
+
+int Aeroport::getNombreTerminal() const {
+    return nombreTerminal;
+}
+
+// Méthode d'affichage des informations de l'aéroport
+void Aeroport::displayInfoAeroport() const {
+    std::cout << "Nom de l'aeroport : " << nom << std::endl;
+    std::cout << "Adresse de l'aeroport : " << adresse << std::endl;
+    std::cout << "Nombre de terminaux : " << nombreTerminal << std::endl;
+}
